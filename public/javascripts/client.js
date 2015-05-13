7ebe3ac39e34cd8285c111783eaef099fac0151d
Chelsea Okey <chelsea.okey@gmail.com>
2015-05-13 14:50:37 -0500

    Calculator works

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -2,29 +2,90 @@
  * Created by chelsea on 5/12/15.
  */
 
-var result = 0;
-var displayed;
+var result = null;
+var displayed = "";
+var operation;
 
-function add(){
-    //result = displayed + input;
-    //return result;
-}
-
-function display(){
-
+function display(input) {
+    $('#window').empty();
+    $('#window').append(input);
 }
 
 
-$(document).ready(function() {
-    $('body').on('click', '.btn', function(){
+$(document).ready(function () {
+    $('body').on('click', '.btn', function () {
         console.log($(this).attr('id'));
         var id = $(this).attr('id');
-
-
-        //if(id == "minus"){
-        //    minus();
-        //} else {
-        //    displayed +=
-        //}
+        console.log("Before anything result = " + result);
+        if(result == null) {
+            if (id == "-") {
+                operation = "subtract";
+                result = parseInt(displayed);
+                displayed = "";
+                console.log("After clicking minus, result = " + result);
+            } else if (id == "+") {
+                operation = "add";
+                result = parseInt(displayed);
+                displayed = "";
+                console.log("After clicking add, result = " + result);
+            } else if (id == "/") {
+                operation = "divide";
+                result = parseInt(displayed);
+                displayed = "";
+                console.log("After clicking divide, result = " + result);
+            } else if (id == "*") {
+                operation = "multiply";
+                result = parseInt(displayed);
+                displayed = "";
+                console.log("After clicking multiply, result = " + result);
+            } else if (id == "=") {
+                display(displayed);
+            } else {
+                displayed += id;
+                display(displayed);
+                console.log("The 1st else ran");
+            }
+        } else {
+            if (id == "-") {
+                result -= parseInt(displayed);
+                displayed = "";
+                console.log("After actually subtracting, result = " + result);
+            } else if (id == "+") {
+                result = parseInt(displayed);
+                displayed = "";
+                console.log("After actually adding, result = " + result);
+            } else if (id == "/") {
+                result = parseInt(displayed);
+                displayed = "";
+                console.log("After actually dividing, result = " + result);
+            } else if (id == "*") {
+                result = parseInt(displayed);
+                displayed = "";
+                console.log("After actually multiplying, result = " + result);
+            } else if (id == "=") {
+                if(operation == "subtract") {
+                    result -= parseInt(displayed);
+                    display(result);
+                } else if(operation == "add") {
+                    result += parseInt(displayed);
+                    display(result);
+                } else if(operation == "multiply") {
+                    result *= parseInt(displayed);
+                    display(result);
+                } else if(operation == "divide") {
+                    result /= parseInt(displayed);
+                    display(result);
+                }
+            } else {
+                displayed += id;
+                display(displayed);
+                console.log("The 2nd else ran");
+            }
+        }
+    });
+    $('#clear').on('click', function() {
+        $('#window').empty();
+        result = null;
+        displayed = "";
     });
 });
