5f7c74f2f4b44c98a31a17b3e8d135a222522bf8
mliq <liquori@gmail.com>
2015-05-14 11:37:08 -0500

    Easy to find bugs but sort of works

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -6,9 +6,15 @@ var result = null;
 var displayed = "";
 var operation;
 
-function display(input) {
+function display(input, id) {
     $('#window').empty();
     $('#window').append(input);
+    if (id == "+" || id == "-" || id == "/" || id == "*" || id == "=") {
+        post(input);
+    }
+}
+
+function post(input) {
     $.ajax({
         url: '/calculation',
         type: 'POST',
@@ -39,6 +45,7 @@ function historyRefresh() {
         },
         success: function (response) {
             console.log(response);
+            $('.results').empty();
             for (var i = 0; i < response.length; i++) {
                 $('.results').prepend("<div class='well-sm well result col-xs-1'>" + response[i].calculation + "</div>");
             }
@@ -50,31 +57,27 @@ function historyRefresh() {
 }
 
 $(document).ready(function () {
+    historyRefresh();
     $('body').on('click', '.btn', function () {
         console.log($(this).attr('id'));
         var id = $(this).attr('id');
-        console.log("Before anything result = " + result);
         if (result == null) {
             if (id == "-") {
                 operation = "subtract";
                 result = parseInt(displayed);
                 displayed = "";
-                console.log("After clicking minus, result = " + result);
             } else if (id == "+") {
                 operation = "add";
                 result = parseInt(displayed);
                 displayed = "";
-                console.log("After clicking add, result = " + result);
             } else if (id == "/") {
                 operation = "divide";
                 result = parseInt(displayed);
                 displayed = "";
-                console.log("After clicking divide, result = " + result);
             } else if (id == "*") {
                 operation = "multiply";
                 result = parseInt(displayed);
                 displayed = "";
-                console.log("After clicking multiply, result = " + result);
             } else if (id == "=") {
                 display(displayed);
             } else if (id == "clear") {
@@ -84,42 +87,38 @@ $(document).ready(function () {
             } else {
                 displayed += id;
                 display(displayed);
-                console.log("The 1st else ran");
             }
         } else {
             if (id == "-") {
                 result -= parseInt(displayed);
                 displayed = "";
-                display(result);
-                console.log("After actually subtracting, result = " + result);
+                display(result, id);
             } else if (id == "+") {
                 result += parseInt(displayed);
                 displayed = "";
-                display(result);
-                console.log("After actually adding, result = " + result);
+                display(result, id);
             } else if (id == "/") {
                 result = parseInt(displayed);
                 displayed /= "";
-                display(result);
-                console.log("After actually dividing, result = " + result);
+                display(result, id);
             } else if (id == "*") {
                 result *= parseInt(displayed);
                 displayed = "";
-                display(result);
+                display(result, id);
                 console.log("After actually multiplying, result = " + result);
             } else if (id == "=") {
                 if (operation == "subtract") {
                     result -= parseInt(displayed);
-                    display(result);
+                    display(result, id);
                 } else if (operation == "add") {
                     result += parseInt(displayed);
-                    display(result);
+                    display(result, id);
                 } else if (operation == "multiply") {
                     result *= parseInt(displayed);
-                    display(result);
+                    display(result, id);
                 } else if (operation == "divide") {
                     result /= parseInt(displayed);
-                    display(result);
+                    display(result, id);
                 }
             } else if (id == "clear") {
                 $('#window').empty();
@@ -128,7 +127,6 @@ $(document).ready(function () {
             } else {
                 displayed += id;
                 display(displayed);
-                console.log("The 2nd else ran");
             }
         }
     });
